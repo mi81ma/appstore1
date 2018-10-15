0550a86723af8a016280ab3be2a7d2baecd6e644
masato <masato@m-mbp.local>
2018-10-15 19:15:49 +0800

    initial Screen Set

--- appstore1/AppDelegate.swift
+++ appstore1/AppDelegate.swift
@@ -15,6 +15,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+
+        let layout = UICollectionViewFlowLayout()
+        let featuredAppsController = FeaturedAppsController(collectionViewLayout: layout)
+
+        window?.rootViewController = UINavigationController(rootViewController: featuredAppsController)
+
         // Override point for customization after application launch.
         return true
     }
--- appstore1/ViewController.swift
+++ appstore1/ViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FeaturedAppsController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        collectionView?.backgroundColor = UIColor.white
     }
 
 
